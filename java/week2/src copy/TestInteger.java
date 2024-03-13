cbb893c8d1d5420240ca8cb0b24e0e17f24e9c8c
Iksang_J <wjddlrtkd0110@naver.com>
2024-03-13 15:50:39 +0900

    2주차

---
+++ java/week2/src copy/TestInteger.java
@@ -0,0 +1,10 @@
+public class TestInteger {
+    public static void main(String args[])
+    {
+      int u_price=-300;
+      int count = 9;
+
+      System.out.printf("%d원짜리 %d개를 사면 %d입니다\n",300,9,300*9);
+      System.out.printf("%10d원짜리 %+10d개를 사면 %10d입니다\n",u_price,count,u_price*count);
+    }
+}
---
+++ java/week2/src/FloatDoubleTest.java
@@ -0,0 +1,10 @@
+public class FloatDoubleTest {
+    public static void main(String args[])
+    {
+        float a = 12345678901234567890.0f;
+        double b = 12345678901234567890.0;
+        System.out.println("float  변수 a의 값은 : "+a);
+        System.out.println("double 변수 b의 값은 : "+b);
+
+    }
+}
