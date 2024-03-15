3b83bb1be71e293f22cf9891630d57f6f0636a2e
Iksang_J <wjddlrtkd0110@naver.com>
2024-03-15 09:21:09 +0900

    2주차

---
+++ java/week2/src copy 2/SimpleIFTest4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SimpleIFTest4 {
+    public static void main(String args[])
+    {
+        Scanner stdin = new Scanner(System.in);
+        System.out.print("성적을 입력하게요 : ");
+        int grade = stdin.nextInt();
+        if (grade >= 90);
+            System.out.println("축하합니다 ");
+            System.out.println("A학점을 취득하셨습니다.");
+        System.out.println("감사합니다");
+    }
+}
