483c5669ac5240d16243478d6b13cad95fec7f30
Iksang_J <wjddlrtkd0110@naver.com>
2024-10-10 15:11:45 +0900

    WEEK05

--- c#/20212316_정익상/WEEK05_02/Form1.Designer.cs
+++ c#/20212316_정익상/WEEK05_02/Form1.Designer.cs
@@ -29,33 +29,114 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 22);
+            this.button1.Location = new System.Drawing.Point(141, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = ">>";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "추가";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "삭제";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "사과",
+            "딸기",
+            "포도",
+            "오랜지",
+            "바나나",
+            "키위"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(122, 64);
+            this.listBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(223, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 79);
+            this.ClientSize = new System.Drawing.Size(362, 114);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "WEEK05_02";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- c#/20212316_정익상/WEEK05_02/Form1.cs
+++ c#/20212316_정익상/WEEK05_02/Form1.cs
@@ -21,5 +21,45 @@ namespace WEEK05_02
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex > -1)
+            {
+                comboBox1.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else MessageBox.Show("콤보박스로 보낼 아이템을 선택해 주세요.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                listBox1.Items.Add(comboBox1.SelectedItem);
+                comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+                comboBox1.Text = "";
+            }
+            else MessageBox.Show("리스트박스로 보낼 아이템을 선택해 주세요.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+            else MessageBox.Show("추가할 아이템을 입력해 주세요.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex > -1)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItems);
+            }
+            else MessageBox.Show("삭제할 아이템을 선택해 주세요.");
+        }
     }
 }
