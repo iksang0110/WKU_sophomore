c1c78fb610fdc9e7d6b040ca7018d15d679c5d3e
Iksang_J <wjddlrtkd0110@naver.com>
2024-11-24 18:08:56 +0900

    week12

---
+++ c#/20212316_정익상/WEEK12_n/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WEEK12_n
+{
+    public partial class Form1 : Form
+    {
+        private int PenWidth = 3;
+        private Color InColor = Color.Blue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, (float)this.PenWidth);
+            SolidBrush solidBrush = new SolidBrush(this.InColor);
+
+            Graphics g = e.Graphics;
+            Point[] points3 = new Point[10]
+                    {
+                        new Point(110, 40),
+                        new Point(125, 91),
+                        new Point(180, 91),
+                        new Point(135, 123),
+                        new Point(152, 172),
+                        new Point(110, 141),
+                        new Point(66, 172),
+                        new Point(82, 122),
+                        new Point(40, 91),
+                        new Point(95, 91)
+                    };
+            e.Graphics.FillClosedCurve((Brush)solidBrush, points3);
+            e.Graphics.DrawClosedCurve(pen, points3);
+        }
+    }
+}
---
+++ c#/20212316_정익상/WEEK12_도형/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WEEK12_n
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle r = new Rectangle(10, 10, 100, 100);
+            g.FillPie(Brushes.Red, r, 0, 90);
+            g.DrawPie(Pens.Black, r, 0, 90);
+        }
+    }
+}
